816a42aa67a3d5c61fad60ff371455b9b64f2982
zhangchi <718558450@qq.com>
2021-02-23 17:03:37 +0800

    生成静态文件 以及文件管理 显示

--- Arica/src/main/java/com/wsafe/arica/controller/ItemController.java
+++ Arica/src/main/java/com/wsafe/arica/controller/ItemController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 /**
  * @Author:Zhangchi
  * @date:2021-02-22 下午4:49
@@ -44,12 +46,62 @@ public class ItemController {
      */
     @RequestMapping("view")
     public String view(Model model,int id) {
-
         Item item = itemService.findById(id);
         model.addAttribute("item", item);
-
         return "view";
     }
+
+    @RequestMapping("itemList")
+    public String itemList(Model model){
+        List<Item> itemList = itemService.findAll();
+        model.addAttribute("items", itemList);
+        return "item_list";
+    }
+
+    @RequestMapping("generate")
+    public String generate(Model model , int id){
+        itemService.generateHTML(id);
+
+        String msg = "文件生成成功，<a href='item"+id+".html' target='_blank'>查看</a>";
+        model.addAttribute("msg", msg);
+        return "success";
+    }
+
+    @RequestMapping("templates")
+    public String templates(){
+        return "templates";
+    }
+
+    /**
+     * 修改模板
+     * @param model
+     * @return
+     */
+    @RequestMapping("editTemplate")
+    public String editTemplate(Model model)throws Exception {
+
+        String tplStr = itemService.getFileTemplateString();
+        model.addAttribute("tplStr",tplStr);
+        return "edit_template";
+    }
+
+    /**
+     * 保存模板
+     * @param model
+     * @param content
+     * @return
+     */
+    @RequestMapping("saveTemplate")
+    public String saveTemplate(Model model,String content)throws Exception {
+
+        itemService.saveFileTemplateString(content);
+
+        String msg = "保存成功。";
+        model.addAttribute("msg", msg);
+        return "success";
+    }
+
+
 
 
 }
--- Arica/src/main/java/com/wsafe/arica/service/ItemService.java
+++ Arica/src/main/java/com/wsafe/arica/service/ItemService.java
@@ -1,10 +1,18 @@
 package com.wsafe.arica.service;
 
+import com.jfinal.kit.Kv;
+import com.jfinal.template.Engine;
+import com.jfinal.template.Template;
 import com.wsafe.arica.entity.Item;
+import com.wsafe.arica.entity.ItemExample;
 import com.wsafe.arica.mapper.ItemDAO;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ClassUtils;
 
 import javax.annotation.Resource;
+import java.io.*;
+import java.util.List;
 
 /**
  * @Author:Zhangchi
@@ -13,16 +21,79 @@ import javax.annotation.Resource;
 @Service
 public class ItemService {
 
+    @Value("${nginx.html.root}")
+    private String templePath;
+
     @Resource
     private ItemDAO itemDAO;
 
     public Item insert(Item item) {
-
         itemDAO.insert(item);
         return item;
     }
 
+    /**
+     * 查询所有页面信息
+     * @return
+     */
+    public List<Item> findAll(){
+        List<Item> items = itemDAO.selectByExample(new ItemExample());
+        return items;
+    }
+
+    /**
+     * ID查询
+     * @param id
+     * @return
+     */
     public Item findById(int id) {
         return itemDAO.selectByPrimaryKey(id);
+    }
+
+    /**
+     * ID查询数据，JFinal生成静态HTML文件
+     * @param id
+     */
+    public void generateHTML(int id) {
+
+        //初始化模板引擎
+        Engine engine = Engine.use();
+        engine.setDevMode(true);
+        engine.setToClassPathSourceFactory();
+
+        Item item = itemDAO.selectByPrimaryKey(id);
+
+        //模板数据渲染
+        Kv kv = Kv.by("item", item);
+        String fileName = "item"+id+".html";
+        String filePath = templePath;
+        //设置文件路径+文件名
+        File file = new File(filePath+fileName);
+        //渲染输出文件
+        Template template = engine.getTemplate("templates/item.html");
+        template.render(kv, file);
+    }
+
+    public String getFileTemplateString() throws Exception {
+        String file = ClassUtils.getDefaultClassLoader().getResource("templates/item.html").getFile();
+        System.out.println(file);
+        BufferedReader reader =  new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+        StringBuffer fileReader = new StringBuffer();
+        String lineStr = reader.readLine();
+        while(lineStr != null){
+            fileReader.append(lineStr).append("\n\r");
+            lineStr = reader.readLine();
+        }
+        reader.close();
+        return fileReader.toString();
+    }
+
+    public void saveFileTemplateString(String content) throws IOException {
+        String file = ClassUtils.getDefaultClassLoader().getResource("templates/item.html").getFile();
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+        writer.write(content);
+        writer.flush();
+        writer.close();
     }
 }
