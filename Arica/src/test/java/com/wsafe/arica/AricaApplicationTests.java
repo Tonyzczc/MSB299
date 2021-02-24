9147b3b32fa5c41836b804b76c6dbfca3e42dd38
zhangchi <718558450@qq.com>
2021-02-24 15:34:03 +0800

    SpringBoot Test

--- Arica/src/test/java/com/wsafe/arica/AricaApplicationTests.java
+++ Arica/src/test/java/com/wsafe/arica/AricaApplicationTests.java
@@ -1,13 +1,27 @@
 package com.wsafe.arica;
 
+import com.wsafe.arica.entity.Item;
+import com.wsafe.arica.service.ItemService;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class AricaApplicationTests {
 
+    @Autowired
+    ItemService itemService;
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	void findAll(){
+        List<Item> all = itemService.findAll();
+        System.out.println(ToStringBuilder.reflectionToString(all));
+    }
 
 }
